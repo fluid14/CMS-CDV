74aaf29b08546a73fc2581ddfac1d08127bf7e02
Jakub Dolata <jdolata5@gmail.com>
2020-04-12 19:30:45 +0200

    add use context

--- src/templates/GlobalTemplate.js
+++ src/templates/GlobalTemplate.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import GlobalStyle from 'theme/GlobalStyle';
 import theme from 'theme/theme';
 import { ThemeProvider } from 'styled-components/macro';
-// import Preloader from '../components/Preloader/Preloader';
 
-const GlobalTemplate = ({ children }) => (
-  <>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      {/*<Preloader/>*/}
-      {children}
-    </ThemeProvider>
-  </>
-);
+class GlobalTemplate extends Component {
+  state = {
+    user: {
+      userName: '',
+      id: '',
+      role: '',
+    },
+  };
+
+  render() {
+    const { children } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    );
+  }
+}
 
 GlobalTemplate.propTypes = {
   children: PropTypes.element.isRequired,
--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-import { Col, Row, Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
+import { Col, Row, Button, FormGroup, Label, Container } from 'reactstrap';
+import { Formik, Form as FormikForm, Field } from 'formik';
+
+import axios from 'axios';
+import context from 'react-router/modules/RouterContext';
 
 const LoginWrap = styled.div`
   width: 100%;
@@ -22,25 +26,49 @@ const LoginView = () => (
       <p className="lead d-block mb-0">Jakub Dolata</p>
       <p className="lead d-block">Filip Baumgart</p>
     </TitleContainer>
-    <Row class="mt-5">
+    <Row className="mt-5">
       <Col md={12}>
-        <Form>
-          <Row form>
-            <Col md={12}>
-              <FormGroup>
-                <Label for="login">Login</Label>
-                <Input type="text" name="login" id="login" />
-              </FormGroup>
-            </Col>
-            <Col md={12}>
-              <FormGroup>
-                <Label for="password">Hasło</Label>
-                <Input type="password" name="password" id="password" />
-              </FormGroup>
-            </Col>
-          </Row>
-          <Button>Zaloguj się</Button>
-        </Form>
+        <Formik
+          initialValues={{ username: '', password: '' }}
+          onSubmit={(values, { setSubmitting }) => {
+            // const api = process.env.API_URL;
+            const { username, password } = values;
+            axios
+              .post('http://127.0.0.1:5000/login', {
+                username: username,
+                password: password,
+              })
+              .then(function(response) {
+                console.log(response);
+              })
+              .catch(function(error) {
+                console.log(error);
+              });
+            setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <FormikForm>
+              <Row form>
+                <Col md={12}>
+                  <FormGroup>
+                    <Label for="username">Login</Label>
+                    <Field type="text" className="form-control" name="username" required />
+                  </FormGroup>
+                </Col>
+                <Col md={12}>
+                  <FormGroup>
+                    <Label for="password">Hasło</Label>
+                    <Field type="password" className="form-control" name="password" required />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Button type="submit" color="info" disabled={isSubmitting}>
+                Zaloguj
+              </Button>
+            </FormikForm>
+          )}
+        </Formik>
       </Col>
     </Row>
   </LoginWrap>
