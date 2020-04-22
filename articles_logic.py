a1132eead60027465b73ea4032bf7e3be96d4f22
fbaumgart <f.m.baumgart@hotmail.com>
2020-04-22 23:00:32 +0200

    Add simple_page and gallery_page functions for new db schema

--- articles_logic.py
+++ articles_logic.py
@@ -32,17 +32,18 @@ def get_all_articles():
     database = Database()
     connection = database.connect()
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM ARTICLES")
+    cursor.execute("SELECT * FROM TEST_ARTICLES")
     rows = cursor.fetchall()
     articles = []
     for row in rows:
-        articles.append(
-            {
-                "id": row[0],
-                "title": row[1],
-                "date": row[2]
-            }
-        )
+        print(row.title)
+        #articles.append(
+         #   {
+          #      "id": row[0],
+          #      "title": row[1],
+          #      "date": row[2]
+          #  }
+        #)
     cursor.close()
     connection.close()
     return {
@@ -65,24 +66,34 @@ def add_article(request):
     database = Database()
     connection = database.connect()
     cursor = connection.cursor()
-    page_content = request['content']
-    user_id = request['user_id']
-    title = request['title']
+    # page_content = request['content']
+    # user_id = request['user_id']
+    # title = request['title']
+    # page_type = request['page_type']
 
-    insert_article_to_articles_table(cursor, title, user_id)
-    article_id = fetch_max_id_from_articles_table(cursor)
-    add_simple_page(cursor, page_content, article_id)
+
+    #insert_article_to_articles_table(cursor, title, user_id)
+    #article_id = fetch_max_id_from_articles_table(cursor)
+    #add_simple_page(request_data=request, cursor=cursor)
+    add_gallery_page(cursor, request)
 
     cursor.close()
     connection.close()
-    logger.info(f"Article id:{article_id} added")
+    #logger.info(f"Article id:{article_id} added")
+
+#
+# def insert_article_to_articles_table(cursor, title, user_id):
+#     creation_date = date.today()
+#     cursor.execute("INSERT INTO TEST_ARTICLES (id, title) VALUES (?, ?)", user_id, title)
+#     cursor.commit()
+#     logger.info(f"Article: title: {title}, creation_date: {creation_date}, user_id: {user_id} added successfully to database.")
 
 
-def insert_article_to_articles_table(cursor, title, user_id):
-    creation_date = date.today()
-    cursor.execute("INSERT INTO ARTICLES (title, creation_date, user_id) VALUES (?, ?, ?)", title, creation_date, user_id)
-    cursor.commit()
-    logger.info(f"Article: title: {title}, creation_date: {creation_date}, user_id: {user_id} added successfully to database.")
+# def old_insert_article_to_articles_table(cursor, title, user_id):
+#     creation_date = date.today()
+#     cursor.execute("INSERT INTO ARTICLES (title, creation_date, user_id) VALUES (?, ?, ?)", title, creation_date, user_id)
+#     cursor.commit()
+#     logger.info(f"Article: title: {title}, creation_date: {creation_date}, user_id: {user_id} added successfully to database.")
 
 
 def fetch_max_id_from_articles_table(cursor):
@@ -92,20 +103,34 @@ def fetch_max_id_from_articles_table(cursor):
     return article_id
 
 
-def add_simple_page(cursor, content, article_id):
-    image = content['image']
-    title = content['title']
-    paragraph = content['paragraph']
-    params = (article_id, image, title, paragraph)
-    cursor.execute("INSERT INTO ARTICLE_SIMPLE_PAGE VALUES (?, ?, ?, ?)", params)
+def add_simple_page(cursor, request_data):
+    user_id = request_data["user_id"]
+    page_type = request_data["page_type"]
+    content = request_data["content"]
+    short_title = content["short_title"]
+    short_description = content["short_description"]
+    creation_date = date.today()
+    preview_image = content["preview_image"]
+    image = content["image"]
+    paragraph = content["paragraph"]
+    params = (user_id, page_type, short_title, short_description, creation_date, preview_image, image, paragraph)
+    cursor.execute("INSERT INTO ARTICLES_PROD (user_id, page_type, short_title, short_description, creation_date, preview_image, image1, paragraph1) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", params)
     cursor.commit()
-    logger.info(f"Article: id:{article_id}, added successfully to database.")
+    logger.info(f"Article added successfully to database.")
 
 
-def add_gallery_page(cursor, content, article_id):
-    hero_image = content['hero_image']
-    hero_image_title = content['hero_image_title']
-    title = content['title']
+def add_gallery_page(cursor, request_data):
+    #TODO:
+    user_id = request_data["user_id"]
+    page_type = request_data["page_type"]
+    content = request_data["content"]
+    short_title = content["short_title"]
+    short_description = content["short_description"]
+    creation_date = date.today()
+    preview_image = content["preview_image"]
+
+    header = content["header"]
+    hero_img = content['hero_img']
     paragraph = content['paragraph']
     image1 = content['image1']
     image2 = content['image2']
@@ -116,18 +141,35 @@ def add_gallery_page(cursor, content, article_id):
     image7 = content['image7']
     image8 = content['image8']
     image9 = content['image9']
-    params = (article_id, hero_image, hero_image_title,
-              title, paragraph,
+    params = (user_id, page_type, short_title,
+              short_description, creation_date, preview_image,
+              hero_img, header, paragraph,
               image1, image2, image3,
               image4, image5, image6,
               image7, image8, image9)
-    cursor.execute("INSERT INTO ARTICLE_GALLERY_PAGE VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", params)
-    #TODO: zrobic tabele w bazie danych
+    cursor.execute("""
+    INSERT INTO ARTICLES_PROD 
+    (user_id, page_type, short_title,
+     short_description, creation_date, preview_image,
+      hero_img, header, paragraph1,
+       image1, image2, image3, 
+       image4, image5, image6, 
+       image7, image8, image9) 
+         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", params)
     cursor.commit()
-    logger.info(f"Article: id:{article_id}, added successfully to database.")
+    logger.info(f"Article added successfully to database.")
 
 
-def add_hero_page(cursor, content, article_id):
+def add_hero_page(cursor, request_data):
+    #TODO:
+    user_id = request_data["user_id"]
+    page_type = request_data["page_type"]
+    content = request_data["content"]
+    short_title = content["short_title"]
+    short_description = content["short_description"]
+    creation_date = date.today()
+    preview_image = content["preview_image"]
+
     hero_image = content['hero_image']
     title = content['title']
     column1 = content['column1']
@@ -136,33 +178,49 @@ def add_hero_page(cursor, content, article_id):
     icon1 = content['icon1']
     icon2 = content['icon2']
     icon3 = content['icon3']
-    params = (article_id, hero_image, title,
+    params = (hero_image, title,
               column1, column2,
               column3, icon1,
               icon2, icon3)
-    cursor.execute("INSERT INTO ARTICLE_HERO_PAGE VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", params)
-    # TODO: zrobic tabele w bazie danych
+    cursor.execute("INSERT INTO ARTICLES_PROD VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", params)
     cursor.commit()
-    logger.info(f"Article: id:{article_id}, added successfully to database.")
+    logger.info(f"Article added successfully to database.")
 
 
-def add_ilustrated_page(cursor, content, article_id):
+def add_ilustrated_page(cursor, request_data):
+    #TODO:
+    user_id = request_data["user_id"]
+    page_type = request_data["page_type"]
+    content = request_data["content"]
+    short_title = content["short_title"]
+    short_description = content["short_description"]
+    creation_date = date.today()
+    preview_image = content["preview_image"]
+
     image = content['image']
     title = content['title']
     paragraph1 = content['paragraph1']
     paragraph2 = content['paragraph2']
     quote = content['quote']
     paragraph3 = content['paragraph3']
-    params = (article_id, image, title,
+    params = (image, title,
               paragraph1, paragraph2,
               quote, paragraph3)
-    cursor.execute("INSERT INTO ARTICLE_ILUSTRATED_PAGE VALUES (?, ?, ?, ?, ?, ?, ?)", params)
-    # TODO: zrobic tabele w bazie danych
+    cursor.execute("INSERT INTO ARTICLES_PROD VALUES (?, ?, ?, ?, ?, ?, ?)", params)
     cursor.commit()
-    logger.info(f"Article: id:{article_id}, added successfully to database.")
+    logger.info(f"Article: added successfully to database.")
 
 
-def add_article_page(cursor, content, article_id):
+def add_article_page(cursor, request_data):
+    #TODO:
+    user_id = request_data["user_id"]
+    page_type = request_data["page_type"]
+    content = request_data["content"]
+    short_title = content["short_title"]
+    short_description = content["short_description"]
+    creation_date = date.today()
+    preview_image = content["preview_image"]
+
     image1 = content['image1']
     image2 = content['image2']
     image3 = content['image3']
@@ -171,10 +229,13 @@ def add_article_page(cursor, content, article_id):
     paragraph3 = content['paragraph3']
     paragraph4 = content['paragraph4']
     title = content['title']
-    params = (article_id, image1, title,
+    params = (image1, title,
               paragraph1, paragraph2, image2,
               paragraph3, image3, paragraph4)
-    cursor.execute("INSERT INTO ARTICLE_ARTICLE_PAGE VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", params)
-    # TODO: zrobic tabele w bazie danych
+    cursor.execute("INSERT INTO ARTICLES_PROD VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", params)
     cursor.commit()
-    logger.info(f"Article: id:{article_id}, added successfully to database.")
+    logger.info(f"Article added successfully to database.")
+
+
+#def check_page_type_and_call_add_function(page_type, cursor, request_data):
+#TODO:
